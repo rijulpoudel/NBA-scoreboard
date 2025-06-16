ddafbf8e765abc130fc13a26248f673314f394d7
Rijul Poudel <rijulpoudel72@gmail.com>
2025-06-16 12:46:40 -0500

    first push

---
+++ index.js
@@ -0,0 +1,20 @@
+let count_home = 0;
+let count_guest = 0;
+
+let score_home = document.getElementById('home')
+let score_guest = document.getElementById('guest')
+
+let add1 = () =>{
+    count_home +=1 
+    score_home.textContent = count_home
+}
+
+let add2 = () =>{
+    count_home +=2 
+    score_home.textContent = count_home
+}
+
+let add3 = () =>{
+    count_home +=3
+    score_home.textContent = count_home
+}
